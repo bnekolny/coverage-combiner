ca67a04de048cd4b84caeb0228df7fa912cc36e1
Brett Nekolny <brett.nekolny@mapmyfitness.com>
2013-08-08 22:36:31 -0600

    Moving more stuff around.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "CoverageCombiner",
-    version = "0.2",
-    packages = find_packages(),
+    name = "coverage_combiner",
+    version = "0.3",
+    packages = ['coverage_combiner'],
 )
